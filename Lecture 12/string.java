fa7fe64f3edd4c0371e9c41d33fd1976ee0a6703
vrundaaa27 <vrundaaa2704@gmail.com>
2026-08-11 14:59:56 +0530

    Add Lecture 12 and Lecture 13 programs

---
+++ Lecture 12/string.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class string {
+    public static void main(String[] args) {
+        
+        String name = "Tony";
+        String fullName = "Tony Stark";
+        String sentence = "My name is Tony Stark";
+
+        System.out.println(name);
+        System.out.println(fullName);
+        System.out.println(sentence);
+    }
+}
