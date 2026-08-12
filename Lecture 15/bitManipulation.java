27e99a0e7de8361317759c101392f0bd56ee68ef
vrundaaa27 <vrundaaa2704@gmail.com>
2026-08-12 14:28:15 +0530

    Lecture 15 updated

---
+++ Lecture 15/bitManipulation.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class bitManipulation{
+    public static void main(String[] args) {
+        
+        int n = 5;
+        int pos = 2;
+        int bitMask = 1<<pos;
+
+        if ((bitMask & n) == 0) {
+            System.out.println("Bit is zero");
+        }else{
+            System.out.println("Bit is one");
+        }
+    }
+}
