2d72d43930d5445a16f4fc76c32ffa792020f62a
vrundaaa27 <vrundaaa2704@gmail.com>
2026-08-15 14:56:51 +0530

    Lecture 17 updated

---
+++ Lecture17/Recursion2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Recursion2 {
+    public static void printNumb(int n){
+        if (n == 6) {
+            return;
+        }
+        System.out.println(n);
+        printNumb(n+1);
+    }
+    public static void main(String[] args) {
+        int n = 0;
+        printNumb(n);
+    }
+}    
