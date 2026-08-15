db3486f2cc936bf94a82b29088f4d95bdf647ff2
vrundaaa27 <vrundaaa2704@gmail.com>
2026-08-15 15:19:00 +0530

    Lecture 17 updated

--- Lecture17/Recursion3.java
+++ Lecture17/Recursion3.java
@@ -1,16 +1,31 @@
 import java.util.*;
 
-public class Recursion3 {
-    public static void printSum(int i, int n, int sum){
-        if (i == n) {
-            sum += i;
+// public class Recursion3 {
+//     public static void printSum(int i, int n, int sum){
+//         if (i == n) {
+//             sum += i;
+//             System.out.println(sum);
+//             return;
+//         }
+//         sum += i;
+//         printSum(i+1, n, sum);;
+//     }
+//     public static void main(String[] args) {
+//         printSum(1, 5, 0);
+//     }
+// }
+
+public class Recursion3{
+    public static void printSum(int a, int b, int sum){
+        if (a == b) {
+            sum += a;
             System.out.println(sum);
             return;
         }
-        sum += i;
-        printSum(i+1, n, sum);;
+        sum += a;
+        printSum(a+1, b, sum);
     }
-    public static void main(String[] args) {
+    public static void main(String[] args){
         printSum(1, 5, 0);
     }
 }
